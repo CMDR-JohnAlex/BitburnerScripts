c4976fb96965ce1fcd54dbee98bbe58fae544740
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-07-29 15:41:59 -0400

    Updated scripts. UpdateServerScripts.js now has the option to try and run with the maximum possible threads.

--- UpdateServerScripts.js
+++ UpdateServerScripts.js
@@ -3,21 +3,26 @@
 /*
 args:
 0: [string] Script to run
-1: [int] Threads to use
+1: [int/"max"] Threads to use
 
-RAM Usage: 4.05GB
+RAM Usage: 4.20GB
 */
 
 export async function main(ns)
 {
 	let i = 0;
 
-	while(i < ns.getPurchasedServerLimit())
+	while (i < ns.getPurchasedServerLimit())
 	{
 		let hostname = "pserv-" + i;
 		ns.killall(hostname, true);
 		ns.scp(ns.args[0], hostname);
-		ns.exec(ns.args[0], hostname, ns.args[1]);
+		let possibleThreads = args[1];
+		if (ns.args[1] == "max")
+		{
+			possibleThreads = ns.getServerMaxRam(hostname) / ns.getScriptRam(ns.args[0], "home");
+		}
+		ns.exec(ns.args[0], possibleThreads, ns.args[1]);
 		i++;
 	}
 }
