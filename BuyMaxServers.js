c7415af6588b08881e89c0434052e3a2ab644bae
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-08-04 09:38:10 -0400

    Updated scripts. Print more info from buy server script.

--- BuyMaxServers.js
+++ BuyMaxServers.js
@@ -17,7 +17,9 @@ export async function main(ns)
     // Iterator we'll use for our loop
     let i = 0;
 
-    ns.print("It costs " + ns.getPurchasedServerCost(ram) + " to buy a server with " + ram + "GB of RAM");
+    ns.print("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
+    ns.tprint("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
+    ns.alert("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
 
     // Continuously try to purchase servers until we've reached the maximum
     // amount of servers
