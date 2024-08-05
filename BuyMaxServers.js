87715f6850f9efb7647181a8d4157ed184ad5ca4
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-08-05 08:53:59 -0400

    Updated scripts. BuyMaxServers.js will no longer buy servers that already exist if re-ran.

--- BuyMaxServers.js
+++ BuyMaxServers.js
@@ -4,35 +4,31 @@
 args:
 0: [int] Amount of RAM to buy
 
-RAM Usage: 4.25GB
+RAM Usage: 6.50GB
 */
 
 export async function main(ns)
 {
-    ns.tail();
+	ns.tail();
 
-    // How much RAM each purchased server will have
-    const ram = ns.args[0];
+	const ram = ns.args[0];
 
-    // Iterator we'll use for our loop
-    let i = 0;
+	ns.print("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
+	ns.tprint("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
+	ns.alert("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
 
-    ns.print("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
-    ns.tprint("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
-    ns.alert("It will cost $" + ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit() + " to buy " + ns.getPurchasedServerLimit() + " servers with " + ram + ".00GB of RAM. ($" + ns.getPurchasedServerCost(ram) + " per server)");
+	let purchasedServerCount = ns.getPurchasedServers().length;
+	for (let i = purchasedServerCount; i < ns.getPurchasedServerLimit(); i)
+	{
+		if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram))
+		{
+			ns.purchaseServer("pserv-" + i, ram);
+			ns.print("Purchased server pserv-" + i + " with " + ram + ".00GB of RAM.");
+			ns.tprint("Purchased server pserv-" + i + " with " + ram + ".00GB of RAM.");
+			ns.alert("Purchased server pserv-" + i + " with " + ram + ".00GB of RAM.");
+			i++;
+		}
 
-    // Continuously try to purchase servers until we've reached the maximum
-    // amount of servers
-    while (i < ns.getPurchasedServerLimit())
-    {
-        // Check if we have enough money to purchase a server
-        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram))
-        {
-            ns.purchaseServer("pserv-" + i, ram);
-            i++;
-        }
-
-        // Make the script wait for a second before looping again.
 		await ns.sleep(5000);
 	}
 }
