320d4fe07cf6f1a9a1010460f0c8accd999b6d91
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-08-09 12:56:00 -0400

    Updated scripts. Really need to work on the file updater. For now, this will do...

--- UpdateFiles.js
+++ UpdateFiles.js
@@ -7,15 +7,24 @@ RAM Usage: 1.60GB
 export async function main(ns)
 {
 	// TODO: Re-write this all, make it fancy and dynamic.
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/HackManager.js", "HackManager.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/GrowAndReply.js", "GrowAndReply.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/HackAndReply.js", "HackAndReply.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/LoopGrow.js", "LoopGrow.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/LoopHack.js", "LoopHack.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/LoopWeaken.js", "LoopWeaken.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/SingleGrow.js", "SingleGrow.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/SingleHack.js", "SingleHack.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/SingleWeaken.js", "SingleWeaken.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/WeakenAndReply.js", "WeakenAndReply.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/StandaloneHackingScript.js", "StandaloneHackingScript.js");
 	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/NetworkInfiltraitor.js", "NetworkInfiltraitor.js");
 	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/Preparer.js", "Preparer.js");
-	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/TempMessageRecorder.js", "TempMessageRecorder.js");
-	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/Template.js", "Template.js");
-	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/UpdateFiles.js", "UpdateFiles.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/IPvGO1.0.js", "IPvGO1.0.js");
 	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/BuyMaxServers.js", "BuyMaxServers.js");
-	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/RemoteWorm.js", "RemoteWorm.js");
 	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/SellAllServers.js", "SellAllServers.js");
-	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/StandaloneHackingScript.js", "StandaloneHackingScript.js");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/UpdateFiles.js", "UpdateFiles.js");
 	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/UpdateServerScripts.js", "UpdateServerScripts.js");
 	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/README.md", "README.txt");
+	ns.wget("https://raw.githubusercontent.com/CMDR-JohnAlex/BitburnerScripts/master/Template.js", "Template.js");
 }
